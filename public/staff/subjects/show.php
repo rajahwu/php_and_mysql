a4696a7b73ac3e9d895c4a53183594b98b84282c
rajahwu <vmradford@gmail.com>
2024-03-04 11:13:04 -0500

    inital commit

---
+++ public/staff/subjects/show.php
@@ -0,0 +1,21 @@
+<?php require_once('../../../private/initialize.php') ?>
+
+
+<?php
+    $id = $_GET['id'] ?? '1';
+    $page_title = 'Show Page'; 
+?>
+    <?php include(SHARED_PATH . '/staff_header.php'); ?>
+    
+    <div id="content">
+    <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php') ?>">&laquo; Back to list</a>
+
+    <div class="page show">
+
+        Page ID: <?php echo h($id); ?>
+
+    </div>
+    </div>
+
+
+    <?php include(SHARED_PATH . '/staff_footer.php'); ?>
