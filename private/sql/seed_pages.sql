0ecb4aa653fe705bf6762328b8909da1726cfb01
rajahwu <vmradford@gmail.com>
2024-03-06 15:28:08 -0500

    add seed data to db

---
+++ private/sql/seed_pages.sql
@@ -0,0 +1,11 @@
+INSERT INTO pages (subject_id, menu_name, position, visible, content)
+VALUES (1, 'Globe Bank', '1', '1', 'Globe Bank is a fictional bank created for the purpose of learning web development. This is a demo site for managing content with a database.'),
+    (1, 'History', '2', '1', 'Globe Bank was founded in 1900, and has been serving the world for over a century.'),
+    (1, 'Leadership', '3', '1', 'Globe Bank has a very dedicated leadership team.'),
+    (1, 'Contact Us', '4', '1', 'Please contact us with any questions or concerns.'),
+    (2, 'Banking', '1', '1', 'We offer a wide range of consumer banking services.'),
+    (2, 'Credit card', '2', '1', 'We provide financial services for small businesses.'),
+    (2, 'Mortgages', '3', '1', 'We offer competitive mortgage rates.'),
+    (3, 'Checking', '1', '1', 'We offer a variety of business checking accounts.'),
+    (3, 'Loans', '2', '1', 'We offer a variety of small business loans.'),
+    (3, 'Merchant', '3', '1', 'We offer a variety of merchant services.');
