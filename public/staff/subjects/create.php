6a6f57358705fef2db44dd1eace71e4778c2ee71
rajahwu <vmradford@gmail.com>
2024-03-07 15:36:45 -0500

    create update subject query function

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -2,20 +2,20 @@
 
 <?php
 
-    require_once('../../../private/initialize.php');
+require_once('../../../private/initialize.php');
 
-    if(is_post_request()) {
-        $menu_name = $_POST['menu_name'] ?? '';
-        $position = $_POST['position'] ?? '';
-        $visible = $_POST['visible'] ?? '';
-    
-        echo "Form parameters<br />";
-        echo "Menu name: " . $menu_name . "<br />";
-        echo "Position: " . $position . "<br />";
-        echo "Visible: " . $visible . "<br />";
-    } else {
-        redirect_to(url_for('/staff/subjects/new.php'));
-    }
+if (is_post_request()) {
+    $menu_name = $_POST['menu_name'] ?? '';
+    $position = $_POST['position'] ?? '';
+    $visible = $_POST['visible'] ?? '';
+
+    $result = insert_subject($menu_name, $position, $visible);
+    $new_id = mysqli_insert_id($db);
+    redirect_to(url_for('/staff/subjects/show.php?id=' . $new_id));
+   
+} else {
+    redirect_to(url_for('/staff/subjects/new.php'));
+}
 
 
 ?>
